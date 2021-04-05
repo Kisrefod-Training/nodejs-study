403f7987c3f341922024a9bf8eb09702214992eb
kisrefod <vanoykkostin@yandex.ru>
2021-04-05 15:51:48 +0300

    fix(config): "var" changed to "const", removed comment

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-var innerHost = process.env.HOST || 'localhost';//Или возьмется значение из окружения, иои localhost - если так писать не принято - скажи - взял это из случайно статьи по сокращениям в js
-var innerPort = process.env.PORT || '1337';
+const innerHost = process.env.HOST || 'localhost';
+const innerPort = process.env.PORT || '1337';
 
 export default {host: innerHost, port: innerPort}
 export const config = {host: innerHost, port: innerPort}
