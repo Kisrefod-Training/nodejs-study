69e6b1e4af469cd9f588b2b4d3f3f3b4060fb1cb
kisrefod <vanoykkostin@yandex.ru>
2021-04-21 15:43:22 +0300

    fix: now filter-test (powered by testcafe) works correctly. But view-test.ts shouldn't be so hardcoded - will be fixed in future patches

--- test/view-test.ts
+++ test/view-test.ts
@@ -9,8 +9,10 @@ import HttpServer from '../src/http-server-class.js';
 describe('Table filter works correctly', () => {
     let testcafeOutput = '';
     const server = new HttpServer();
+    let outputArray:Array<string>;
 
-    before(async done => {
+    before(async function () {
+        this.timeout(20000);
         const spiedServer = mockito.spy(server);
 
         mockito.when(spiedServer.getData()).thenReturn(parsedDataPromise);
@@ -23,19 +25,44 @@ describe('Table filter works correctly', () => {
         ));
 
         process.on('out', response => {
-            testcafeOutput = response;
+            testcafeOutput += `${response}\n`;
         });
 
-        await once(process, 'out');
-        done();
+        await once(process, 'close');
+        const filterIndex = testcafeOutput.indexOf(' Filter works correctly');
+
+        outputArray = testcafeOutput.slice(filterIndex).split('\n');
     });
     after(done => {
         server.stopServer();
         done();
     });
 
-    it('qweqwe', () => {
-        console.log(testcafeOutput);
-        assert.deepStrictEqual(typeof testcafeOutput, 'string');
-    }).timeout(10000);
+    it('All data printed out', () => {
+        assert.deepStrictEqual(outputArray[1], ' √ All data printed out');
+    });
+    it('PRs printed out correctly', () => {
+        assert.deepStrictEqual(outputArray[2], ' √ PRs printed out correctly');
+    });
+    it('Commits printed out correctly', () => {
+        assert.deepStrictEqual(outputArray[3], ' √ Commits printed out correctly');
+    });
+    it('Filter is case-insensitive - \'a\' and \'A\' returns the same', () => {
+        assert.deepStrictEqual(outputArray[4], ' √ Filter is case-insensitive - \'a\' and \'A\' returns the same');
+    });
+    it('Filter is field-insensitive - \'b\' viewed in all fields', () => {
+        assert.deepStrictEqual(outputArray[5], ' √ Filter is field-insensitive - \'b\' viewed in all fields');
+    });
+    it('Filter works correctly with requests of different lengths', () => {
+        assert.deepStrictEqual(outputArray[6], ' √ Filter works correctly with requests of different lengths');
+    });
+    it('Filter works correctly with multi-word requests', () => {
+        assert.deepStrictEqual(outputArray[7], ' √ Filter works correctly with multi-word requests');
+    });
+    it('Filter works correctly with null result requests', () => {
+        assert.deepStrictEqual(outputArray[8], ' √ Filter works correctly with null result requests');
+    });
+    it('Filter works correctly with null result requests', () => {
+        assert.deepStrictEqual(outputArray[8], ' √ Filter works correctly with null result requests');
+    });
 });
