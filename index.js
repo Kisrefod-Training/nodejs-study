d42b54b2d75653f4786d58ddfe4e092a23e718e6
kisrefod <vanoykkostin@yandex.ru>
2021-04-21 10:21:40 +0300

    fix: removed comments for linter and excess ".then()"

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 import esMain from 'es-main';
-import { host, port } from './src/config.js';// eslint-disable-line import/extensions
-import HttpServer from './src/httpServerClass.js';// eslint-disable-line import/extensions
+import { host, port } from './src/config.js';
+import HttpServer from './src/httpServerClass.js';
 
 const server = new HttpServer();
 
 export default server;
 
-if (esMain(import.meta)) {
-    // Для линтера добавлен парсер babel-eslint, чтобы он не ругался на import.meta
-    server.startServer(host, port).then();
-}
+if (esMain(import.meta)) server.startServer(host, port);
+
